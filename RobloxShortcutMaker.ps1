4374659735e971687b01bb7d9fe7d07a08b91032
xaizko <145681428+xaizko@users.noreply.github.com>
2025-10-18 17:03:43 -0400

    Script to make shortcut

---
+++ RobloxShortcutMaker.ps1
@@ -0,0 +1,5 @@
+Write-Host "Welcome to the Roblox shortcut maker!"
+
+$Name = Read-Host -Prompt "Enter Desired Name: "
+$GameId = Read-Host -Prompt "Enter Game ID: "
+$IconPath = Read-Host -Prompt "Enter (optional) path to icon (Press enter for none): "
