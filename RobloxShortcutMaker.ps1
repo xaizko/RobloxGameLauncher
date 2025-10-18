d867e23db820f0be53c6c58674395a311c94ad6c
xaizko <145681428+xaizko@users.noreply.github.com>
2025-10-18 17:31:19 -0400

    Implemented creating launch script
    Added script logic to create a launcher for Roblox games.

--- RobloxShortcutMaker.ps1
+++ RobloxShortcutMaker.ps1
@@ -1,5 +1,13 @@
 Write-Host "Welcome to the Roblox shortcut maker!"
 
-$Name = Read-Host -Prompt "Enter Desired Name: "
-$GameId = Read-Host -Prompt "Enter Game ID: "
-$IconPath = Read-Host -Prompt "Enter (optional) path to icon (Press enter for none): "
+$Name = Read-Host -Prompt "Enter Desired Name"
+$GameId = Read-Host -Prompt "Enter Game ID"
+$IconPath = Read-Host -Prompt "Enter (optional) path to icon (Press enter for none)"
+
+# Current Directory of Script
+$ScriptDir = $PSScriptRoot
+
+# Create launcher script
+$gameLaunchScript = $Name + "Launch.bat"
+$BatScriptPath = "$ScriptDir\Launch Scripts\$gameLaunchScript"
+Set-Content -Path $BatScriptPath -Value "@echo off`nstart roblox://placeId=$GameId"
